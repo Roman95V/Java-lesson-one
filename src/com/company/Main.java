cabb70f1ee90d0178d1c878ec47015498aa452de
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-11-03 20:00:26 +0200

    one commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int num1 = 6;
+        int num2 = -9;
+        int num3 = 50;
+        int num4 = 2;
+
+       /* Scanner in = new Scanner(System.in);
+        System.out.print("Input a first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Input a second number: ");
+        int num2 = in.nextInt();
+        System.out.print("Input a third number: ");
+        int num3 = in.nextInt();
+        System.out.print("Input a fourth number: ");
+        int num4 = in.nextInt();*/
+
+        if (num1 <= num2 && num1 <= num3 && num1 <= num4)
+        {
+            System.out.println("Number " + num1 + " smallest ");
+        }
+
+        else if (num2 <= num1 && num2 <= num3 && num2 <= num4)
+        {
+            System.out.println("Number " + num2 + " smallest ");
+        }
+
+        else if (num3 <= num1 && num3 <= num2 && num3 <= num4)
+        {
+
+            System.out.println("Number " + num3 + " smallest ");
+        }
+
+        else if (num4 <= num1 && num4 <= num2 && num4 <= num3)
+        {
+            System.out.println("Number " + num4 + " smallest ");
+        }
+
+    }
+}
